04c32ad5fe8ecede7db7aa28c35e2353d503c5bc
alexanderleo0 <alnikitin3@gmail.com>
2023-03-17 17:25:38 +0300

    start branch

--- Sources/Networking/APIs.swift
+++ Sources/Networking/APIs.swift
@@ -14,7 +14,7 @@ import Foundation
 public typealias OpenAPIClient = OpenAPIClientAPI
 
 open class OpenAPIClientAPI {
-    public static var basePath = "https://api.thecatapi.com/v1"
+    public static var basePath = "https://rickandmortyapi.com/api"
     public static var customHeaders: [String: String] = [:]
     public static var credential: URLCredential?
     public static var requestBuilderFactory: RequestBuilderFactory = URLSessionRequestBuilderFactory()
